ca819cb41dedd577037ac6cdd3d7dc928cf82b8f
TXXXW-WQ <tokiwaryu001@gmail.com>
2025-11-30 22:08:07 +0900

    Knapsack 完了

--- D - Knapsack 1.py
+++ D - Knapsack 1.py
@@ -8,10 +8,15 @@ import io
 import sys
 
 _INPUT = """\
-3
-10 40 70
-20 50 80
-30 60 90
+6 15
+6 5
+5 6
+6 4
+6 6
+3 5
+7 2
+
+
 """
 sys.stdin = io.StringIO(_INPUT)
 
@@ -19,5 +24,11 @@ sys.stdin = io.StringIO(_INPUT)
 
 n,w = map(int,input().split())
 dp = [[0] * (w+1) for _ in range(n+1)]
-for _ in range(n):
-  
+for i in range(1,n+1):
+  a, b = map(int, input().split())
+  for j in range(w+1):
+    if j < a:
+      dp[i][j] = dp[i-1][j]
+    else:
+      dp[i][j] = max(dp[i-1][j], dp[i-1][j-a] + b)
+print(dp[n][w])
