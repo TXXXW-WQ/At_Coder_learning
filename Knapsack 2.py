43bbc4cde2f4e084dea08387d2749493c478585a
TXXXW-WQ <tokiwaryu001@gmail.com>
2025-12-03 22:28:18 +0900

    1,2でwの制約が違う

--- Knapsack 2.py
+++ Knapsack 2.py
@@ -6,13 +6,10 @@ import io
 import sys
 
 _INPUT = """\
-6 15
-6 5
-5 6
-6 4
-6 6
-3 5
-7 2
+3 8
+3 30
+4 50
+5 60
 
 """
 sys.stdin = io.StringIO(_INPUT)
@@ -21,11 +18,11 @@ sys.stdin = io.StringIO(_INPUT)
 
 n,w = map(int, input().split())
 dp = [[0] * (w+1) for _ in range(n+1)]
-for i in range(1,n):
+for i in range(1,n+1):
   wi, vi = map(int, input().split())
   for j in range(w+1):
     if j < wi:
-      dp[i+1][j] = dp[i][j]
+      dp[i][j] = dp[i-1][j]
     else:
-      dp[i+1][j] = max(dp[i][j], dp[i][j-wi] + vi)
+      dp[i][j] = max(dp[i-1][j], dp[i-1][j-wi] + vi)
 print(dp[n][w])
